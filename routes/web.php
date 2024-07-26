e68ed362afef49058909a7e14c13ba1a061b2437
AmmarDab3an <ammar.dabaan99@gmail.com>
2024-07-27 01:39:38 +0300

    removing verified middleware

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,14 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['verified', 'auth'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::middleware(['verified', 'auth', 'admin'])->group(function () {
+Route::middleware(['auth', 'admin'])->group(function () {
     Route::resource('users', UserController::class);
     Route::resource('students', StudentController::class);
 });
 
-Route::middleware('verified', 'auth')->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
